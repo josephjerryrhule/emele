04c7b49efe0d9f95dd0654285c91b3154da4d414
joseph <josephjerryrhule@gmail.com>
2023-12-26 17:09:31 +0000

    updated with hover animation

--- app/components/header/header.tsx
+++ app/components/header/header.tsx
@@ -4,8 +4,11 @@ import React from "react";
 const HeaderComponent = () => {
   return (
     <div className="flex w-full items-center justify-between">
-      <div className="logo-area">
-        <Link href="/">Header Logo</Link>
+      <div className="logo-area text-[30px] group">
+        <Link href="/" className="flex items-center gap-5">
+          <div className="w-[20px] h-[20px] bg-white rounded-full transition-all duration-300 ease-in-out group-hover:rounded-[0px]"></div>
+          <span>E-A-H</span>
+        </Link>
       </div>
       <div className="navlinksarea flex items-center gap-5 font-semibold">
         <Link href="#home">Home</Link>
--- app/components/home/about.tsx
+++ app/components/home/about.tsx
@@ -1,9 +1,11 @@
 import gsap from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
+import Image from "next/image";
 import React, { useLayoutEffect, useRef } from "react";
 
 const About = () => {
   const abouttext = useRef(null);
+  const text = useRef(null);
 
   useLayoutEffect(() => {
     gsap.registerPlugin(ScrollTrigger);
@@ -25,6 +27,34 @@ const About = () => {
       { opacity: 1, y: 50 },
       "+=1"
     );
+
+    const animation = gsap.to(".emeleimg", {
+      paused: true,
+      opacity: 1,
+      scale: 1,
+    });
+
+    const handleMouseOver = () => {
+      animation.play(); // Play the animation when mouseover occurs
+    };
+
+    const handleMouseOut = () => {
+      animation.reverse(); // Reverse the animation when mouseout occurs
+    };
+
+    // Add event listeners to the text element
+    if (text.current) {
+      text.current.addEventListener("mouseover", handleMouseOver);
+      text.current.addEventListener("mouseout", handleMouseOut);
+    }
+
+    // Clean up event listeners
+    return () => {
+      if (text.current) {
+        text.current.removeEventListener("mouseover", handleMouseOver);
+        text.current.removeEventListener("mouseout", handleMouseOut);
+      }
+    };
   }, []);
 
   return (
@@ -33,18 +63,29 @@ const About = () => {
       ref={abouttext}
       id="about"
     >
-      <div className="bg-emelered rounded-[20px] p-[6px_32px] font-bold w-fit text-[20px] leading-normal">
+      <div className="bg-emelered rounded-[20px] p-[6px_32px] font-bold w-fit text-[20px] leading-normal relative z-[1]">
         About
       </div>
-      <h2>
-        I am Emele Arthur-Hayford.
+      <h2 className="relative z-[1]">
+        I am{" "}
+        <span ref={text} className="emele-bg">
+          Emele Arthur-Hayford
+        </span>
+        .
         <br />
         A Medical Laboratory Scientist.
         <br />A Digital Marketer, Founder of WMF.
       </h2>
-      <div className="absolute bottom-0 right-0 text-[12px] font-normal uppercase">
+      <div className="absolute bottom-0 right-0 text-[12px] font-normal uppercase z-[1]">
         Scroll
       </div>
+      <Image
+        src="/emele.jpg"
+        alt="Emele"
+        width={100}
+        height={100}
+        className="absolute w-full h-full object-cover emeleimg z-0 opacity-0 scale-[0.98]"
+      />
     </div>
   );
 };
