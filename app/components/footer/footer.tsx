e14f7dead983d6fbf19ab3d0147f6c16779cf90b
joseph <josephjerryrhule@gmail.com>
2023-12-30 03:32:00 +0000

    updated footer links

--- app/components/footer/footer.tsx
+++ app/components/footer/footer.tsx
@@ -1,7 +1,20 @@
+import Link from "next/link";
 import React from "react";
 
 const FooterComponent = () => {
-  return <div>FooterComponent</div>;
+  return (
+    <div className="w-full flex items-center text-white text-2xl pt-10 pb-10 uppercase gap-5">
+      <Link href="https://www.instagram.com/just_emele_/" target="_blank">
+        Instagram
+      </Link>
+      <Link
+        href="https://www.linkedin.com/in/emele-arthur-hayford-574161206"
+        target="_blank"
+      >
+        Linkedin
+      </Link>
+    </div>
+  );
 };
 
 export default FooterComponent;
