3c7bffff613f066fa727f2e525ab1b75e3743773
joseph <josephjerryrhule@gmail.com>
2024-01-01 12:24:42 +0000

    updated backend API URL

--- next.config.js
+++ next.config.js
@@ -3,8 +3,8 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: "http",
-        hostname: "backend.emelearthurhayford.com",
+        protocol: "https",
+        hostname: "emele.joeydutch.com",
       },
     ],
   },
